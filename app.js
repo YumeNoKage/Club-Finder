2eb035402d7a7c632b6e8f660e37cb4c8cde6131
YumeNoKage <ericopratama2003@gmail.com>
2020-04-13 17:08:23 +0700

    fix materi npm & add webpack  css-loader

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-import main from './src/script/view/main.js';
+import main from "./src/script/view/main.js";
+import "./src/script/component/app-bar/app-bar.js";
+import "./src/script/component/search-bar/search-bar.js";
+import "./src/script/component/club-list/club-list.js";
 
-document.addEventListener("DOMContentLoaded",main);
+document.addEventListener("DOMContentLoaded", main);
--- src/script/view/main.js
+++ src/script/view/main.js
@@ -1,42 +1,29 @@
-import DataSource from '../data/data-source.js';
+import "../component/club-list/club-list.js";
+import "../component/search-bar/search-bar.js";
+import DataSource from "../data/data-source.js";
 
 const main = () => {
-    const searchElement = document.querySelector("#searchElement");
-    const buttonSearchElement = document.querySelector("#searchButtonElement");
-    const clubListElement = document.querySelector("#clubList");
+  const searchElement = document.querySelector("search-bar");
+  const clubListElement = document.querySelector("club-list");
 
-    const onButtonSearchClicked = () => {
-        DataSource.searchClub(searchElement.value)
-        .then(renderResult)
-        .catch(fallbackResult)
-    };
+  const onButtonSearchClicked = async () => {
+    try {
+      const result = await DataSource.searchClub(searchElement.value);
+      renderResult(result);
+    } catch (message) {
+      fallbackResult(message);
+    }
+  };
 
-    const renderResult = results => {
-        clubListElement.innerHTML = "";
-        results.forEach(club => {
-            // const name = club.name;
-            // const fanArt = club.fanArt;
-            // const description = club.description;
-            const { name, fanArt, description } = club;
+  const renderResult = (results) => {
+    clubListElement.clubs = results;
+  };
 
-            const clubElement = document.createElement("div");
-            clubElement.setAttribute("class", "club");
+  const fallbackResult = (message) => {
+    clubListElement.renderError(message);
+  };
 
-            clubElement.innerHTML = `<img class="fan-art-club" src="${fanArt}" alt="Fan Art">
-                <div class="club-info">
-                <h2>${name}</h2>\n
-                <p>${description}</p>
-                </div>`;
-            clubListElement.appendChild(clubElement);
-        })
-    };
-
-    const fallbackResult = message => {
-        clubListElement.innerHTML = "";
-        clubListElement.innerHTML += `<h2 class="placeholder">${message}</h2>`
-    };
-
-    buttonSearchElement.addEventListener("click", onButtonSearchClicked);
+  searchElement.clickEvent = onButtonSearchClicked;
 };
 
 export default main;
